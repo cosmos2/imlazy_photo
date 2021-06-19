6693add27d257c9d4ca53171e6c46dede844c34c
cosmos2 <playnstop.s@gmail.com>
2021-06-19 13:36:08 +0900

    [CHG] distinguish img / video

--- imlazy_photo/imlazy_photo.py
+++ imlazy_photo/imlazy_photo.py
@@ -2,12 +2,21 @@ import os
 import time
 import shutil
 
+
+IMG_FORMAT = ['jpg', 'jpeg', 'gif', 'heic']
+VIDEO_FORMAT = ['mp4', 'avi']
+VALID_FORMAT = IMG_FORMAT + VIDEO_FORMAT
+
+
 def sort_photo():
     c_path = os.getcwd()
     file_list = os.listdir(c_path)
     result = {}
 
     for file_name in file_list:
+        if os.path.isdir(file_name) or file_name.split('.')[1].lower() not in VALID_FORMAT:
+            continue
+            
         etime = os.path.getmtime(file_name)
         created = f"{c_path}/{time.strftime('%Y%m%d', time.localtime(etime))}"
 
@@ -26,10 +35,24 @@ def make_dir_move_file(path_dict):
             if not os.path.exists(path):
                 os.makedirs(path)
                 _move_file(file_list, path)
-        except OSError:
+        except OSError as e:
             _move_file(file_list, path)
 
 def _move_file(file_list, path):
+    has_video_dir = False if not os.path.exists(f'{path}/video_clips') else True
+
+
     for file_name in file_list:
-        print(file_name)
-        shutil.move(file_name, f'{path}/{file_name}')
+        if file_name.split('.')[1].lower() in IMG_FORMAT:
+            pass
+            shutil.move(file_name, f'{path}/{file_name}')
+        elif file_name.split('.')[1].lower() in VIDEO_FORMAT:
+
+            if not has_video_dir:
+                os.makedirs(f'{path}/video_clips')
+                has_video_dir = True
+
+            shutil.move(file_name, f'{path}/video_clips/{file_name}')
+
+f_path = sort_photo()
+make_dir_move_file(f_path)
